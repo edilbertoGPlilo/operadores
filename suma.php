dac16185f1f07fa7254fad84b724f458c12d9312
edilbertoGPlilo <135733656+edilbertoGPlilo@users.noreply.github.com>
2023-07-27 08:48:03 -0500

    Add files via upload

---
+++ suma.php
@@ -0,0 +1,57 @@
+<?php
+// variables
+$n1=0;
+$n2=0;
+$s=0;
+//
+if(isset($_POST["btncalcular"])){
+
+$n1 = (int)$_POST["txtn1"];
+$n2 = (int)$_POST["txtn2"];
+// salida
+$s = $n1+$n2;
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+      <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="suma.php" method="post">
+
+       <table width="300" border=1>
+        <tr >
+            <td> calcula de suma de dos numeros</td>
+        </tr>
+        <tr>
+            <td>numero 1 </td>
+            <td><input name="txtn1" type="text" id="txtn1" value="<?=$n1?>"></td>
+        </tr>
+        <tr >
+            <td>numero 2 </td>
+            <td><input name="txtn2" type="text" id="txtn2" value="<?=$n2?>"></td>
+        </tr>
+        <tr>
+            <td>suma</td>
+            <td>
+              <input name="txts"type="text" id="txts"value="<?=$s?>">  
+            </td>
+            <tr>
+            <td> </td>
+            <td>
+                <input name="btncalcular" type="submit" id="btncacular" value="cacular">
+             </td>
+            </tr>
+        </tr>
+       </table>
+
+     </form>
+</body>
+</html>
